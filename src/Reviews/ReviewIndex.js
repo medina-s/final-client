e312f91b9119aac0bf1f03450db025ffe5b55e79
EngelkenRyan <engelkenryan@gmail.com>
2021-07-15 21:31:47 -0400

    added splash page as well as sidebar

---
+++ src/Reviews/ReviewIndex.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = (props) => {
+    return(
+        <div>
+            Welcome to our Blue Badge Project
+            Courtesy of Ryan, Marla, and Medina!
+        </div>
+    )
+}
+
+export default Home;
