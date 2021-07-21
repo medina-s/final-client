a95ab330f7783f2b91c30994dae9333cedfd265a
EngelkenRyan <engelkenryan@gmail.com>
2021-07-20 20:57:59 -0400

    working on reviewall

--- src/Reviews/ReviewCreate.js
+++ src/Reviews/ReviewCreate.js
@@ -9,13 +9,13 @@ const ReviewCreate = (props) => {
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(props.token);
+        console.log(props.sessionToken);
         fetch(`http://localhost:3000/review/create`, {
             method: 'POST',
             body: JSON.stringify({review: {movie: movie, date: date, feedback: feedback}}),
             headers: new Headers({
                 'Content-type': 'application/json',
-                'Authorization': `Bearer ${props.token}`
+                'Authorization': `Bearer ${props.sessionToken}`
             })
         }).then((res)=> res.json())
         .then((logData) => {
@@ -23,30 +23,29 @@ const ReviewCreate = (props) => {
             setMovie('');
             setDate('');
             setFeedback('');
-            props.fetchReviews();
         })
     }
 
     return(
-        <>
+        <div className="moviereview">
         <h3>Leave your feedback</h3>
         <Form onSubmit={handleSubmit}>
             <FormGroup>
                 <Label htmlFor="movie"/>
-                <Input name="movie" value={movie} onChange={(e)=> setMovie(e.target.value)} />
+                <Input name="movie" placeholder="Movie" value={movie} onChange={(e)=> setMovie(e.target.value)} />
             </FormGroup>
             <FormGroup>
                 <Label htmlFor="date"/>
-                <Input name="date" value={date} onChange={(e)=> setDate(e.target.value)}>
+                <Input name="date" placeholder="Date" value={date} onChange={(e)=> setDate(e.target.value)}>
                 </Input>
             </FormGroup>
             <FormGroup>
                 <Label htmlFor="feedback"/>
-                <Input name="feedback" value={feedback} onChange={(e)=> setFeedback(e.target.value)}/>
+                <Input name="feedback" placeholder="Enter your review" value={feedback} onChange={(e)=> setFeedback(e.target.value)}/>
             </FormGroup>
             <Button type="submit">Click to submit</Button>
         </Form>
-        </>
+        </div>
     )
 }
 
