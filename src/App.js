5a1bc55755cdbae3b7c79cec1a7d548e66ba82de
Medina88 <Msydyka@iu.edu>
2021-07-27 20:04:33 -0400

    resolving some conflicts for pulling from master origin

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const clearToken = () => {
 }
 
 const protectedViews = () => {
-  return (sessionToken === localStorage.getItem('token') ? <ReviewIndex token={sessionToken}/>: <Auth updateToken={updateToken}/>)
+  return (sessionToken === localStorage.getItem('token') ? <> </>: <Auth updateToken={updateToken}/>)
 }
 
   return (
